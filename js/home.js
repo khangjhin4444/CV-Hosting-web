cbaf187c3b4dbcd0898b6fdacc4f93dc9d28fd51
Khang <nldkhang@gmail.com>
2025-03-09 16:33:24 +0700

    change carousel

--- js/home.js
+++ js/home.js
@@ -1,7 +1,29 @@
 const carousel = document.querySelector('.carousel');
-        document.getElementById('prevBtn').addEventListener('click', () => {
-            carousel.scrollBy({ left: -320, behavior: 'smooth' });
-        });
-        document.getElementById('nextBtn').addEventListener('click', () => {
-            carousel.scrollBy({ left: 320, behavior: 'smooth' });
-        });
+// document.getElementById('prevBtn').addEventListener('click', () => {
+//     carousel.scrollBy({ left: -320, behavior: 'smooth' });
+// });
+// document.getElementById('nextBtn').addEventListener('click', () => {
+//     carousel.scrollBy({ left: 320, behavior: 'smooth' });
+// });
+let isDragging  = false, startX, startScrollLeft;
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add('dragging');
+    startX = e.pageX;
+    startScrollLeft = carousel.startScrollLeft;
+}
+
+const dragging = (e) => {
+    if (!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove('dragging');
+}
+
+carousel.addEventListener('mousemove', dragging);
+carousel.addEventListener('mousedown', dragStart);
+document.addEventListener('mouseup', dragStop)
