65a749b0c95b9d3fe1420493a1806781897fccc6
Khang <nldkhang@gmail.com>
2025-03-09 17:11:36 +0700

    complete carousel

--- js/home.js
+++ js/home.js
@@ -1,22 +1,39 @@
 const carousel = document.querySelector('.carousel');
-// document.getElementById('prevBtn').addEventListener('click', () => {
-//     carousel.scrollBy({ left: -320, behavior: 'smooth' });
-// });
-// document.getElementById('nextBtn').addEventListener('click', () => {
-//     carousel.scrollBy({ left: 320, behavior: 'smooth' });
-// });
+const arrowBtns = document.querySelectorAll('.wrapper i')
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
+
+
 let isDragging  = false, startX, startScrollLeft;
+
+let cardPreView = Math.round(carousel.offsetWidth / firstCardWidth)
+
+// carouselChildrens.slice(-cardPreView).reverse().forEach(card => {
+//     card.insertAdjacentHTML("afterbegin", card.outerHTML)
+// })
+
+// carouselChildrens.slice(0, cardPreView).forEach(card => {
+//     card.insertAdjacentHTML("beforeend", card.outerHTML)
+// })
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        carousel.scrollLeft += btn.id ==='left' ? -firstCardWidth : firstCardWidth;
+    })
+})
+
 
 const dragStart = (e) => {
     isDragging = true;
-    carousel.classList.add('dragging');
+    carousel.classList.add("dragging");
     startX = e.pageX;
-    startScrollLeft = carousel.startScrollLeft;
+    startScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) => {
     if (!isDragging) return;
     carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+
 }
 
 const dragStop = () => {
